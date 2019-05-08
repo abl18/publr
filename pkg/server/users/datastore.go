5adf4a9f1fef8773a0b2a726b7c57237bcdf80cd
Ahmad Nurus S <prksu.sh@gmail.com>
2019-05-08 21:51:52 +0700

    Implement users service
    Skip the search method for now

--- pkg/server/users/datastore.go
+++ pkg/server/users/datastore.go
@@ -17,13 +17,18 @@ package users
 import (
 	"database/sql"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/golang/protobuf/ptypes"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	usersv1alpha1 "github.com/prksu/publr/pkg/api/users/v1alpha1"
 	"github.com/prksu/publr/pkg/storage/database"
 )
 
 // UserDatastore interface
 type UserDatastore interface {
-	List() ([]*usersv1alpha1.User, error)
+	List(sitedomain string, start, offset int) ([]*usersv1alpha1.User, error)
 	Create(sitedomain string, user *usersv1alpha1.User) error
 	Get(sitedomain, username string) (*usersv1alpha1.User, error)
 	Update(sitedomain, username string, user *usersv1alpha1.User) error
@@ -36,21 +41,134 @@ type datastore struct {
 	DB *sql.DB
 }
 
-// NewDatastore create new users service datastore instance
-func NewDatastore() UserDatastore {
+// NewUserDatastore create new users service datastore instance
+func NewUserDatastore() UserDatastore {
 	ds := new(datastore)
 	ds.DB = database.NewDatabase().Connect()
 	return ds
 }
 
-func (ds *datastore) List() ([]*usersv1alpha1.User, error) { return nil, nil }
+func (ds *datastore) List(sitedomain string, start, offset int) ([]*usersv1alpha1.User, error) {
+	var users []*usersv1alpha1.User
 
-func (ds *datastore) Create(sitedomain string, user *usersv1alpha1.User) error { return nil }
+	sqlquery := `
+		SELECT u.email, u.username, u.fullname, su.role, u.createtime, u.updatetime
+		FROM users AS u 
+		LEFT JOIN site_users AS su on u.username=su.user_username
+		WHERE su.site_domain=?
+		LIMIT ?, ?
+	`
 
-func (ds *datastore) Get(sitedomain, username string) (*usersv1alpha1.User, error) { return nil, nil }
+	rows, err := ds.DB.Query(sqlquery, sitedomain, start, offset)
+	if err != nil {
+		return nil, err
+	}
 
-func (ds *datastore) Update(sitedomain, username string, user *usersv1alpha1.User) error { return nil }
+	defer rows.Close()
+	for rows.Next() {
+		var user usersv1alpha1.User
+		var createTime mysql.NullTime
+		var updateTime mysql.NullTime
+		if err := rows.Scan(&user.Email, &user.Username, &user.Fullname, &user.Role, &createTime, &updateTime); err != nil {
+			return nil, err
+		}
+		user.CreateTime, _ = ptypes.TimestampProto(createTime.Time)
+		user.UpdateTime, _ = ptypes.TimestampProto(updateTime.Time)
+		users = append(users, &user)
+	}
 
-func (ds *datastore) Delete(sitedomain, username string) error { return nil }
+	return users, nil
+}
+
+func (ds *datastore) Create(sitedomain string, user *usersv1alpha1.User) error {
+	tx, err := ds.DB.Begin()
+	if err != nil {
+		return err
+	}
+	{
+		stmt, err := tx.Prepare("INSERT INTO users (email, username, fullname) VALUES (?, ?, ?)")
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+		defer stmt.Close()
+		if _, err := stmt.Exec(user.Email, user.Username, user.Fullname); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	{
+		stmt, err := tx.Prepare("INSERT INTO site_users (user_username, site_domain, role) VALUES (?, ?, ?)")
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+		defer stmt.Close()
+		if _, err := stmt.Exec(user.Username, sitedomain, user.Role); err != nil {
+			tx.Rollback()
+			if err.(*mysql.MySQLError).Number == 1062 {
+				return status.Error(codes.AlreadyExists, "user already exists")
+			}
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+func (ds *datastore) Get(sitedomain, username string) (*usersv1alpha1.User, error) {
+	user := new(usersv1alpha1.User)
+	var createTime mysql.NullTime
+	var updateTime mysql.NullTime
+	var sqlquery = `
+		SELECT u.email, u.username, u.fullname, su.role, u.createtime, u.updatetime
+		FROM users AS u 
+		LEFT JOIN site_users AS su on u.username=su.user_username
+		WHERE username=? AND su.site_domain=?
+	`
+	if err := ds.DB.QueryRow(sqlquery, username, sitedomain).
+		Scan(&user.Email, &user.Username, &user.Fullname, &user.Role, &createTime, &updateTime); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Error(codes.NotFound, "user not found")
+		}
+		return nil, err
+	}
+
+	user.CreateTime, _ = ptypes.TimestampProto(createTime.Time)
+	user.UpdateTime, _ = ptypes.TimestampProto(updateTime.Time)
+	return user, nil
+}
+
+func (ds *datastore) Update(sitedomain, username string, user *usersv1alpha1.User) error {
+	var sqlquery = `
+		UPDATE users AS u LEFT JOIN site_users AS su on u.username=su.user_username
+		SET u.username=?, u.fullname=?, su.role=?
+		WHERE u.username=? AND su.site_domain=?
+	`
+	stmt, err := ds.DB.Prepare(sqlquery)
+	if err != nil {
+		return err
+	}
+
+	if _, err := stmt.Exec(user.Username, user.Fullname, user.Role, username, sitedomain); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ds *datastore) Delete(sitedomain, username string) error {
+	var sqlquery = `
+		DELETE users FROM users
+		LEFT JOIN site_users on users.username=site_users.user_username
+		WHERE site_users.site_domain=? AND users.username=?
+	`
+
+	if _, err := ds.DB.Exec(sqlquery, sitedomain, username); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (ds *datastore) Search(query string) ([]*usersv1alpha1.User, error) { return nil, nil }
--- pkg/server/users/server.go
+++ pkg/server/users/server.go
@@ -16,12 +16,15 @@ package users
 
 import (
 	"context"
+	"encoding/json"
+	"strings"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	usersv1alpha1 "github.com/prksu/publr/pkg/api/users/v1alpha1"
+	"github.com/prksu/publr/pkg/util"
 )
 
 // Users service
@@ -33,40 +36,145 @@ var (
 
 // Server implement usersv1alpha1.UserServiceServer.
 type Server struct {
-	DS UserDatastore
+	User      UserDatastore
+	PageToken util.PageToken
 }
 
 // NewServer create new users service server.
 // returns usersv1alpha1.SiteServiceServer
 func NewServer() usersv1alpha1.UserServiceServer {
 	server := new(Server)
-	server.DS = NewDatastore()
+	server.User = NewUserDatastore()
+	server.PageToken = util.NewPageToken()
 	return server
 }
 
 // ListUser handler method
 func (s *Server) ListUser(ctx context.Context, req *usersv1alpha1.ListUserRequest) (*usersv1alpha1.UserList, error) {
-	return nil, status.Error(codes.Unimplemented, "not implement yet")
+	parent := req.Parent
+	start, err := s.PageToken.Parse(req.PageToken)
+	if err != nil {
+		return nil, err
+	}
+	offset := int(req.PageSize)
+	if offset == 0 {
+		offset = 10
+	}
+
+	sitedomain := strings.Split(parent, "/")[1]
+	users, err := s.User.List(sitedomain, start, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, i := range users {
+		i.Name = strings.Join([]string{parent, "users", i.Username}, "/")
+	}
+
+	var nextPageToken string
+	if len(users) == offset {
+		nextPageToken = s.PageToken.Generate(start + offset)
+	}
+
+	res := new(usersv1alpha1.UserList)
+	res.Users = users
+	res.NextPageToken = nextPageToken
+	return res, nil
 }
 
 // CreateUser handler method
 func (s *Server) CreateUser(ctx context.Context, req *usersv1alpha1.CreateUserRequest) (*usersv1alpha1.User, error) {
-	return nil, status.Error(codes.Unimplemented, "not implement yet")
+	parent := req.Parent
+	user := req.User
+
+	if user.Username == "" {
+		return nil, status.Error(codes.InvalidArgument, "username is required")
+	}
+
+	if user.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if user.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	sitedomain := strings.Split(parent, "/")[1]
+	username := user.Username
+
+	// TODO: Check if the site is really exists.
+
+	if err := s.User.Create(sitedomain, user); err != nil {
+		return nil, err
+	}
+
+	res, err := s.User.Get(sitedomain, username)
+	if err != nil {
+		return nil, err
+	}
+
+	res.Name = strings.Join([]string{parent, "users", username}, "/")
+	return res, nil
 }
 
 // GetUser handler method
 func (s *Server) GetUser(ctx context.Context, req *usersv1alpha1.GetUserRequest) (*usersv1alpha1.User, error) {
-	return nil, status.Error(codes.Unimplemented, "not implement yet")
+	name := req.Name
+	sitedomain := strings.Split(name, "/")[1]
+	username := strings.Split(name, "/")[3]
+	res, err := s.User.Get(sitedomain, username)
+	if err != nil {
+		return nil, err
+	}
+
+	res.Name = name
+	return res, nil
 }
 
 // UpdateUser handler method
 func (s *Server) UpdateUser(ctx context.Context, req *usersv1alpha1.UpdateUserRequest) (*usersv1alpha1.User, error) {
-	return nil, status.Error(codes.Unimplemented, "not implement yet")
+	name := req.Name
+	sitedomain := strings.Split(name, "/")[1]
+	username := strings.Split(name, "/")[3]
+
+	res, err := s.User.Get(sitedomain, username)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.User.Role == 0 && res.Role != 3 {
+		res.Role = 0
+	} else if res.Role == 3 && req.User.Role != 3 {
+		return nil, status.Error(codes.InvalidArgument, "owner role cannot be update")
+	}
+
+	b, err := json.Marshal(req.User)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(b, res); err != nil {
+		return nil, err
+	}
+
+	if err := s.User.Update(sitedomain, username, res); err != nil {
+		return nil, err
+	}
+
+	res.Name = strings.Join([]string{"sites", sitedomain, "users", res.Username}, "/")
+	return res, nil
 }
 
 // DeleteUser handler method
 func (s *Server) DeleteUser(ctx context.Context, req *usersv1alpha1.DeleteUserRequest) (*empty.Empty, error) {
-	return nil, status.Error(codes.Unimplemented, "not implement yet")
+	name := req.Name
+	sitedomain := strings.Split(name, "/")[1]
+	username := strings.Split(name, "/")[3]
+
+	if err := s.User.Delete(sitedomain, username); err != nil {
+		return nil, err
+	}
+
+	return new(empty.Empty), nil
 }
 
 // SearchUser handler method
