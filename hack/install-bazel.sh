a0f9803494bed461ee37057db2cac437d6be288d
Ahmad Nurus S <prksu.sh@gmail.com>
2019-05-19 06:55:55 +0700

    Update travis

---
+++ hack/install-bazel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2019 Publr Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+BAZEL_VERSION=0.18.0
+OS=$(go env GOHOSTOS)
+
+wget -O bazel-install.sh \
+  "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-${OS}-x86_64.sh"
+chmod +x bazel-install.sh && ./bazel-install.sh --user && rm -f bazel-install.sh
